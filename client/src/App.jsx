d92b265476bf598fbfb9d635b149ddd595e10c27
abelaleb <abelaleb@users.noreply.github.com>
2024-11-26 23:08:26 +0300

    Add frontend with map

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// import MapComponent from "./MapComponent"; // Assuming MapComponent.js is in the same folder
+
+function App() {
+  return (
+    <div>
+      <h1>Travel Log</h1>
+    </div>
+  );
+}
+
+export default App;
